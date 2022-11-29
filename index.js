0b8465ca16aa2719530ba001b547422edfec4a6f
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-29 14:26:52 +0545

    second commit

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
+// creating a new element anchor tag --> a
+
 let newA = document.createElement("a");
 let actualLink = document.getElementsByClassName("container")[0];
 
+// adding a title to the anchor tag --> a = "title"
+
 let title = document.getElementById("title");
 
-title.addEventListener("keypress", (event) => {
-  title = document.getElementById("title").value;
-  if (event.key === "Enter") {
-    alert(title);
-  }
-  newA.innerText = `${title}`;
-});
+function highTitle() {
+  title.addEventListener("keypress", (event) => {
+    title = document.getElementById("title").value;
+    if (event.key === "Enter") {
+      alert(title);
+    }
+    newA.innerText = `${title}`;
+    return title;
+  });
+  return title;
+}
+
+// adding link to the anchor tag --> a = "link"
 
 let webLink = document.getElementById("link");
 
-webLink.addEventListener("keypress", (event) => {
+webLink.addEventListener("keypress", function bmLink(event) {
   webLink = document.getElementById("link").value;
   if (event.key === "Enter") {
     alert(webLink);
   }
-  actualLink.append(newA);
-  newA.setAttribute("href", `${webLink}`);
+
+  return webLink;
 });
+
+let valve = highTitle();
+console.log(valve);
+// console.log(bmTitle());
+
+// actualLink.append(newA);
+// newA.setAttribute("href", `${webLink}`);
