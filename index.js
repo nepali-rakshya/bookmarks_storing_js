4e9a27ba25f478052db377b06b03ece7f2b65062
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-30 11:54:19 +0545

    data display

--- index.js
+++ index.js
@@ -14,24 +14,22 @@ function valueL() {
 }
 
 function storeValues() {
-  for (let i = 1; i--; ) {
-    let web = document.getElementsByTagName("tbody")[0];
-    let newTR = document.createElement("tr");
-    let newTD1 = document.createElement("td");
-    let newTD2 = document.createElement("td");
-    let newA = document.createElement("a");
-    newA.innerText = valueTT();
-    newA.setAttribute("href", valueL());
-    newA.setAttribute("target", "_blank");
-    newTD1.append(newA);
-    newTD2.innerText = valueL();
-    newTR.append(newTD1, newTD2);
-    web.append(newTR);
-    let tableHead = document.getElementsByTagName("th")[0];
-    let tableHead2 = document.getElementsByTagName("th")[1];
-    tableHead.classList.add("bottomBorder");
-    tableHead2.classList.add("bottomBorder");
-  }
+  let web = document.getElementsByTagName("tbody")[0];
+  let newTR = document.createElement("tr");
+  let newTD1 = document.createElement("td");
+  let newTD2 = document.createElement("td");
+  let newA = document.createElement("a");
+  newA.innerText = valueTT();
+  newA.setAttribute("href", valueL());
+  newA.setAttribute("target", "_blank");
+  newTD1.append(newA);
+  newTD2.innerText = valueL();
+  newTR.append(newTD1, newTD2);
+  web.append(newTR);
+  let tableHead = document.getElementsByTagName("th")[0];
+  let tableHead2 = document.getElementsByTagName("th")[1];
+  tableHead.classList.add("bottomBorder");
+  tableHead2.classList.add("bottomBorder");
 }
 
 let button = document.getElementsByTagName("button")[0];
